7aa508fedf5fac65551862d1fd52e1cd88068de5
erictc95 <maxerictc95@hotmail.com>
2026-02-25 16:19:20 +0100

    Version 7 - Modified previous exercise NamesStringMain: Create List of names and strings sorted by reverse. Exercise 7 Level 1.

---
+++ src/main/java/level1/lambdasnstreams/exercise7/NameStringMain.java
@@ -0,0 +1,26 @@
+package level1.lambdasnstreams.exercise7;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NameStringMain {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+
+        names.add("Mozzie");
+        names.add("Neal Caffrey");
+        names.add("Petter Burke");
+        names.add("Alex");
+        names.add("Elise");
+        names.add("Mathieu Keller");
+
+        List<String> result = sortString(names);
+        System.out.println(result);
+    }
+
+    public static List<String> sortString(List<String> names) {
+        return names.stream().sorted(Comparator.comparing(String::length).reversed()).collect(Collectors.toList());
+    }
+}
