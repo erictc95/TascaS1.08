f756b5b9f965c29044cb3eb03a68bc60a5c15980
erictc95 <maxerictc95@hotmail.com>
2026-02-26 12:52:27 +0100

    Version 13 - Exercise 4 Level 2. Created first method part, Alphabetical Sort.

---
+++ src/main/java/level2/manipulatefunctionaldata/exercise4/NameStringMain.java
@@ -0,0 +1,28 @@
+package level2.manipulatefunctionaldata.exercise4;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NameStringMain {
+    public static void main(String[] args) {
+        List<String> films = new ArrayList<>();
+
+        films.add("Now You See Me");
+        films.add("The Mask");
+        films.add("Toy Story");
+        films.add("Psycho");
+        films.add("007 James Bond: Die Another Day");
+        films.add("Transformers");
+
+        System.out.println(alphabeticalSort(films));
+
+    }
+
+    public static List<String> alphabeticalSort(List<String> films) {
+        return films.stream().sorted(Comparator.comparing(film -> film.charAt(0))).collect(Collectors.toList());
+    }
+
+
+}
